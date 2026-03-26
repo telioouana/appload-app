dee4c47ea1f3b4654fdb2a2c545f945bb3c65ec1
telioouana <telio.ouana@gmail.com>
2026-03-26 19:53:32 +0200

    redirection bouce fix

--- src/app/(user)/u/redirect/page.tsx
+++ src/app/(user)/u/redirect/page.tsx
@@ -1,5 +1,5 @@
-import { headers } from "next/headers"
 import { redirect } from "next/navigation"
+import { cookies, headers } from "next/headers"
 
 import { auth } from "@/backend/auth"
 
@@ -7,12 +7,20 @@ import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
 
 export default async function Page({ searchParams }: { searchParams: Promise<{ callback: string }> }) {
     const { callback } = await searchParams
+    const sessionCookies = await cookies()
 
     const session = await auth.api.getSession({
         headers: await headers()
     })
 
     if (!session) {
+        const dont_remember = sessionCookies.get("better-auth.dont_remember")
+        const session_token = sessionCookies.get("better-auth.session_token")
+
+        if (dont_remember || session_token) {
+            sessionCookies.delete("better-auth.dont_remember")
+            sessionCookies.delete("better-auth.session_token")
+        }
         redirect("/sign-in")
     }
 
